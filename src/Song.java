918aa3e2527f5bbbe9803f8147feb7e45fe98820
halla7 <32346308+halla7@users.noreply.github.com>
2017-09-27 20:28:42 -0400

    song class

---
+++ src/Song.java
@@ -0,0 +1,40 @@
+// just a comment
+public class Song {
+	private String name, artist;
+	private float playtime;
+
+	
+	public Song(String mname, String martist, float mplaytime) {
+		String name=mname,artist=martist;
+		float playtime=mplaytime;
+    }
+	
+	public String toString(){
+		String mString="Song: "+name+", artist: "+artist+", playtime: "+playtime;
+		return mString;
+		
+	}
+	
+	public double getPlayTime(){
+		return playtime;
+	}
+	
+	public String getArtist(){
+		return artist;
+	}
+	
+	public String getSongName(){
+		return name;
+	}
+	
+	public boolean equals(Song s){
+		if (s.getArtist()!=this.getArtist())
+			return false;
+		if (s.getSongName()!=this.getSongName())
+			return false;
+		if (s.getPlayTime()!=this.getPlayTime())
+			return false;
+		return true;
+	}
+
+}
