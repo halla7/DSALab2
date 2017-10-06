c513efd3fcdd2581ceb1af884a2ca0d57ce82e03
halla7 <32346308+halla7@users.noreply.github.com>
2017-10-06 16:35:02 -0400

    Add files via upload
    My original Song class

---
+++ Song.java
@@ -0,0 +1,40 @@
+// text added 10/6/17
+public class Song {
+	private String name, artist;
+	private float playtime;
+
+	
+	public Song(String mname, String martist, float mplaytime) {
+		String name=mname,artist=martist;
+		float playtime=mplaytime;
+    }
+	
+	public String toString(){
+		String mString="Song: "+name+", artist: "+artist+", playtime: "+playtime;
+		return mString;
+		
+	}
+	
+	public double getPlayTime(){
+		return playtime;
+	}
+	
+	public String getArtist(){
+		return artist;
+	}
+	
+	public String getSongName(){
+		return name;
+	}
+	
+	public boolean equals(Song s){
+		if (s.getArtist()!=this.getArtist())
+			return false;
+		if (s.getSongName()!=this.getSongName())
+			return false;
+		if (s.getPlayTime()!=this.getPlayTime())
+			return false;
+		return true;
+	}
+
+}
